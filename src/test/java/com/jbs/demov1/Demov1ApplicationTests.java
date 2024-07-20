5d2f708ccec6067aeec80cde5cd41ceee7d303e3
SoumyajitMondal7 <soumyajitmodal2003@gmail.com>
2024-07-20 18:22:06 +0530

    First git push

---
+++ src/test/java/com/jbs/demov1/Demov1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbs.demov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demov1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
