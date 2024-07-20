f6eceb2bee2bb1be243f4a275579ef88253f5a85
SoumyajitMondal7 <soumyajitmodal2003@gmail.com>
2024-07-20 18:50:50 +0530

    Second commit

--- src/main/java/com/jbs/demov1/Demov1Application.java
+++ src/main/java/com/jbs/demov1/Demov1Application.java
@@ -9,6 +9,7 @@ public class Demov1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demov1Application.class, args);
 		System.out.println("springboot application started...");
+		System.out.println("Another confirmation of execution...");
 	}
 
 }
